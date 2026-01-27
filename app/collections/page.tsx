1c5fd40c05cfdc5ea5a67d4ff29dfb64d1a9a324
jahidulkanchan <mdj524156@gmail.com>
2026-01-26 23:57:41 -0800

    Crud learing

--- app/collections/page.tsx
+++ app/collections/page.tsx
@@ -1,14 +1,31 @@
 import React from "react";
+import { IProduct } from "../api/products/models/Product";
 
 export default async function page() {
-    const data = await fetch(`${process.env.BASE_URL}/products`)
-    const {products} = await data.json()
-    console.log(products)
+  const data = await fetch(`${process.env.BASE_URL}/products`, {
+    cache: "no-store",
+  });
+  const { products } = await data.json();
+  console.log(products);
   return (
     <>
       <section>
-        <div className="container flex justify-center items-center min-h-screen">
-          I am new products page
+        <div className="container grid grid-cols-3 w-full gap-10 min-h-screen">
+          {products.length > 0 ? (
+            <>
+              {products.map((product: IProduct, index: number) => (
+                <div
+                  key={index}
+                  className="card bg-slate-200 p-5 text-black rounded-2xl"
+                >
+                  <h2>{product.name}</h2>
+                  <h2>{product.price}</h2>
+                </div>
+              ))}
+            </>
+          ) : (
+            <p className="flex justify-center items-center min-h-screen text-center col-span-3">No Product Found</p>
+          )}
         </div>
       </section>
     </>
