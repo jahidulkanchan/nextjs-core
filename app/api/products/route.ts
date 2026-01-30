ef5e6c40bd9bd28648c4160dfca0f579e2d551a0
jahidulkanchan <mdj524156@gmail.com>
2026-01-30 08:59:59 +0600

    add to cahceing

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,7 +1,7 @@
 import connectDB from "@/app/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 import Product, { IProduct } from "./models/Product";
-import { revalidateTag } from "next/cache";
+import { revalidatePath, revalidateTag } from "next/cache";
 
 export async function POST(request: NextRequest) {
   await connectDB();
@@ -23,7 +23,7 @@ export async function POST(request: NextRequest) {
     });
 
     await newProduct.save();
-
+    revalidatePath('/products')
     revalidateTag('products', 'default')
 
     return NextResponse.json({
