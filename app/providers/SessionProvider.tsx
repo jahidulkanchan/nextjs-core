98111cfba32c0676018b2a3d443f9ab374326a18
jahidulkanchan <mdj524156@gmail.com>
2026-02-05 14:23:52 +0600

    Try to add session provider

---
+++ app/providers/SessionProvider.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { SessionProvider } from "next-auth/react";
+import React from "react";
+
+export default function AuthProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = null;
+  return (
+    <>
+      <SessionProvider session={session}>{children}</SessionProvider>
+    </>
+  );
+}
