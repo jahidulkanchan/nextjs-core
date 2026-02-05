fb5ea42043a7f3f4f9065d732a62f9bc7adbbd1c
jahidulkanchan <mdj524156@gmail.com>
2026-02-05 14:18:01 +0600

    conditional middleware done

--- proxy.ts
+++ proxy.ts
@@ -1,10 +1,14 @@
-import { NextResponse, NextRequest } from 'next/server'
- 
+import { NextResponse, NextRequest } from "next/server";
+
 export function proxy(request: NextRequest) {
-    console.log(request)
-  return NextResponse.redirect(new URL('/', request.url))
+  // console.log(request);
+  const user = false;
+  if (!user) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+  return NextResponse.next();
 }
- 
+
 export const config = {
-  matcher: '/about/:path*',
-}
+  matcher: "/products/add",
+};
